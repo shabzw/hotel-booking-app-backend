26d67687c05ca836ae5c5b82f3cd757a9a1506dc
codisho <codisho@users.noreply.github.com>
2024-04-08 15:00:11 +0530

    after configuring es rollup

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default {
   input: 'index.mjs',
   output: {
     file: 'dist/bundle.js',
-    format: 'es',
+    format: 'cjs',
   },
   plugins: [
     nodeResolvePlugin(), // Use the explicitly imported plugin
